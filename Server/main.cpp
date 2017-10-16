476d3f4f2c19a426384f5dbbc219a9ca85378171
CaifeiYang <hedgehogcaifei@outlook.com>
2017-10-16 16:58:11 +0800

    Update main.cpp

--- Server/main.cpp
+++ Server/main.cpp
@@ -2,12 +2,6 @@
 #include "MessageType.h"
 
 
-#include <cppconn/driver.h>
-#include <cppconn/exception.h>
-#include <cppconn/resultset.h>
-#include <cppconn/statement.h>
-
-
 int main()
 {
 	Application app(6666, 10);
