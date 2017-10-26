c8349d46e92605b8b44a710dd1d7bc35a2daeee2
CaifeiYang <hedgehogcaifei@outlook.com>
2017-10-26 16:41:24 +0800

    Update MessageType.h

--- Client/MessageType.h
+++ Client/MessageType.h
@@ -4,29 +4,28 @@
 
 #define DEBUG 1
 
-// ¶¨ÒåÏûÏ¢ÀàĞÍ
+// å®šä¹‰æ¶ˆæ¯ç±»å‹
 enum MessageType
 {
-	TYPE_SIGNIN,				// µÇÂ¼ÏûÏ¢ÀàĞÍ
-	TYPE_SIGNUP,				// ×¢²áÏûÏ¢ÀàĞÍ
-	TYPE_SIGNOUT,				// µÇ³öÏûÏ¢ÀàĞÍ
-	TYPE_USER,					// ÓÃ»§ÏûÏ¢ÀàĞÍ
-	TYPE_MATCH,					// Æ¥ÅäÏûÏ¢ÀàĞÍ
-	TYPE_FIGHT,					// ½»Õ½ÏûÏ¢ÀàĞÍ
-	TYPE_CHAT,					// Chat message
+	TYPE_SIGNIN,				// ç™»å½•æ¶ˆæ¯ç±»å‹
+	TYPE_SIGNUP,				// æ³¨å†Œæ¶ˆæ¯ç±»å‹
+	TYPE_SIGNOUT,				// ç™»å‡ºæ¶ˆæ¯ç±»å‹
+	TYPE_USER,				// ç”¨æˆ·æ¶ˆæ¯ç±»å‹
+	TYPE_MATCH,				// åŒ¹é…æ¶ˆæ¯ç±»å‹
+	TYPE_FIGHT,				// äº¤æˆ˜æ¶ˆæ¯ç±»å‹
+	TYPE_CHAT,				// Chat message
 	TYPE_NONE
 };
 
 
-// ¶¨ÒåÊı¾İÀàĞÍ
+// å®šä¹‰æ•°æ®ç±»å‹é•¿åº¦
+#define MAX_DATA_SIZE	1024		// max data size
 
-#define MAX_DATA_SIZE	1024	// max data size
-
-/* Êı¾İ°üÍ·¶¨Òå */
+/* æ•°æ®åŒ…å¤´å®šä¹‰ */
 typedef struct data_header
 {
-	MessageType msgType;		// ÏûÏ¢ÀàĞÍ
-	unsigned int dataSize;		// Êı¾İ°üÖĞÕæÊµÊı¾İµÄ³¤¶È
+	MessageType msgType;		// æ¶ˆæ¯ç±»å‹
+	unsigned int dataSize;		// æ•°æ®åŒ…ä¸­çœŸå®æ•°æ®çš„é•¿åº¦
 
 	data_header& operator = (const data_header& _data)
 	{
@@ -39,11 +38,11 @@ typedef struct data_header
 	}
 }DATA_HEADER;
 
-/* Êı¾İ°üÀàĞÍ¶¨Òå */
+/* æ•°æ®åŒ…ç±»å‹å®šä¹‰ */
 typedef struct data_package
 {
-	DATA_HEADER dataHeader;		// Êı¾İ°üÍ·
-	char data[MAX_DATA_SIZE];	// ÕæÊµÊı¾İ
+	DATA_HEADER dataHeader;		// æ•°æ®åŒ…å¤´
+	char data[MAX_DATA_SIZE];	// çœŸå®æ•°æ®
 
 	data_package& operator = (const data_package& _data)
 	{
@@ -56,7 +55,7 @@ typedef struct data_package
 	}
 }DATA_PACKAGE;
 
-/* ÏûÏ¢×´Ì¬¸üĞÂ */
+/* æ¶ˆæ¯çŠ¶æ€æ›´æ–° */
 typedef struct msg_state
 {
 	// username
