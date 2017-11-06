554db07857b6fd7faa6e2f1f417918323a96e0a5
CaifeiYang <hedgehogcaifei@outlook.com>
2017-11-06 11:33:13 +0800

    Update Cache.cpp

--- Server/UCache/Cache.cpp
+++ Server/UCache/Cache.cpp
@@ -3,8 +3,17 @@
 std::list<Socket*> Cache::cSockets;
 std::list<virtualThread*> Cache::cThreads;
 
-// »º´æcSocket->username
+// Cache the user of online
+std::unordered_set<std::string> Cache::onlineUsers;
+
+// Cache username->cSocket
 std::unordered_map<std::string, Socket*> Cache::cMaps;
 
-// »º´æÆ¥Åä³É¹¦µÄusername->username
+// Cache the user of matching successfully(username->username)
 std::unordered_map<std::string, std::string> Cache::cUsers;
+
+// Cache the user of being matching
+std::unordered_set<std::string> Cache::isForMatchU;
+
+// Cache the fight users
+std::unordered_map<std::string, std::string> Cache::fightUsers;
