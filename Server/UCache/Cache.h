a0d7fb2a5968edc9da483b65799cfc50e660a420
CaifeiYang <hedgehogcaifei@outlook.com>
2017-11-06 11:32:53 +0800

    Update Cache.h

--- Server/UCache/Cache.h
+++ Server/UCache/Cache.h
@@ -3,6 +3,7 @@
 #include <string>
 #include <list>
 #include <unordered_map>
+#include <unordered_set>
 #include "../Network/Socket.h"
 #include "../Threads/virtualThread.h"
 
@@ -13,9 +14,18 @@ public:
 	static std::list<Socket*> cSockets;
 	static std::list<virtualThread*> cThreads;
 
-	// »º´æusername->cSocket
+	// Cache the user of online
+	static std::unordered_set<std::string> onlineUsers;
+
+	// Cache username->cSocket
 	static std::unordered_map<std::string, Socket*> cMaps;
 
-	// »º´æÆ¥Åä³É¹¦µÄusername->username
+	// Cache the user of matching successfully(username->username)
 	static std::unordered_map<std::string, std::string> cUsers;
+
+	// Cache the user of being matching
+	static std::unordered_set<std::string> isForMatchU;
+
+	// Cache the fight users
+	static std::unordered_map<std::string, std::string> fightUsers;
 };
